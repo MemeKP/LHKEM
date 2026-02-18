ea0022468ed9a401ffaac9985c7862e1eb86f4a8
itspornpailin <pornpailin_j@cmu.ac.th>
2026-02-18 18:48:46 +0700

    feat(workshop-reg): register Workshopregistration schema in module

--- backend/src/workshopregistrations/workshopregistrations.module.ts
+++ backend/src/workshopregistrations/workshopregistrations.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { WorkshopregistrationsService } from './workshopregistrations.service';
 import { WorkshopregistrationsController } from './workshopregistrations.controller';
+import { Workshopregistration, WorkshopregistrationSchema } from './schemas/workshopregistration.schema';
 
 @Module({
+  imports: [
+    /* Injecting the Mongoose model into this module scope */
+    MongooseModule.forFeature([
+      { name: Workshopregistration.name, schema: WorkshopregistrationSchema }
+    ])
+  ],
   controllers: [WorkshopregistrationsController],
   providers: [WorkshopregistrationsService],
 })
